74d34467030b5a000f059eb005adb0a8d15453c5
Abhimanyu Patel <abhimanyu.patel1991@gmail.com>
2015-03-24 10:42:48 +0100

    Animation of moving to border is working
    TODO:
    - flexbox index page
    - handle dragging of collapsed nav-box
    —> better alternative than hiding div OR move box further down/right if
    it’s hidden
    —>  doesn’t let you release the collapsed nav-box: maybe also look into
    dragging just hamburger icon

--- Ex1/base/navigation.js
+++ Ex1/base/navigation.js
@@ -16,11 +16,88 @@
         var makeAnimation = function () {
             if(!isSticky) {
 
+                // window dimensions
                 var height = $(window).height();
-                var widht = $(window).width();
+                var width = $(window).width();
+                
+                // border positions
                 var posTop = nav.position().top;
                 var posLeft = nav.position().left;
 
+                // center positions
+                var centerX = posLeft+nav.width()/2;
+                var centerY = posTop+nav.height()/2;
+
+                /*
+                console.log("height "+height);
+                console.log("width "+width);
+                console.log("posTop "+posTop);
+                console.log("posLeft "+posLeft);
+                console.log("centerX "+centerX);
+                console.log("centerY "+centerY);
+                */
+
+                // to figure out in which quadrant box is located
+                var diffHeight = centerY - (height - centerY);
+                var diffWidth = centerX - (width - centerX);
+
+                // distance to borders
+                var spaceRight = width-centerX;
+                var spaceBottom = height-centerY;
+
+                // positions to move the box
+                var rightBorder = width - nav.width();
+                var bottomBorder = height - nav.height();
+
+
+                // could be done nicer with array + switch but it works!
+                if (diffHeight < 0 && diffWidth < 0) {
+                    // 1st quadrant
+                    if (centerX <= centerY) {
+                        nav.animate({
+                            left: "0"
+                        }, 400);
+                    } else {
+                        nav.animate({
+                            top: "0"
+                        }, 400);
+                    }
+                } else if (diffHeight < 0 && diffWidth > 0) {
+                    // 2nd quadrant
+                    if (spaceRight <= centerY) {
+                        nav.animate({
+                            left: rightBorder
+                        }, 400);
+                    } else {
+                        nav.animate({
+                            top: "0"
+                        }, 400);
+                    }
+                } else if (diffHeight > 0 && diffWidth > 0) {
+                    // 3rd quadrant
+                    if (spaceBottom <= spaceRight) {
+                        nav.animate({
+                            top: bottomBorder
+                        }, 400);
+                    } else {
+                        nav.animate({
+                            left: rightBorder
+                        }, 400);
+                    }
+                } else if (diffHeight > 0 && diffWidth < 0) {
+                    // 4th quadrant
+                    if (centerX <= spaceBottom) {
+                        nav.animate({
+                            left: "0"
+                        }, 400);
+                    } else {
+                        nav.animate({
+                            top: bottomBorder
+                        }, 400);
+                    }
+                }
+
+                /*
                 var animationObj = {
 
                 };
@@ -39,7 +116,8 @@
 
 
                 //make nice animation to neares border
-                nav.animate(animationObj, 5000)
+                //nav.animate(animationObj, 5000)
+                */
             }
         };
 
@@ -80,9 +158,11 @@
         });
 
 
+        // if box hidden, then drag hamburger icon
         nav.mousedown(function (e) {
             dragging = nav;
         });
+
         nav.mouseup(function () {
             dragging = null;
             makeAnimation();
@@ -90,28 +170,4 @@
 
 
     })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }());
