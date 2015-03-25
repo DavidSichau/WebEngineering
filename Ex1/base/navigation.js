c66b531c2e24983fa0b4661c4391932204b9026e
David Sichau <git@sichau.eu>
2015-03-25 15:53:28 +0100

    better handling of hamburger

--- Ex1/base/navigation.js
+++ Ex1/base/navigation.js
@@ -28,71 +28,143 @@
                 // center positions
                 var centerX = posLeft+nav.width()/2;
                 var centerY = posTop+nav.height()/2;
+                //center position of hamburger icon
+                var centerXH = posLeft+nav.find('.nav-menu').height()/2;
+                var centerYH = posTop+ nav.find('.nav-menu').height()/2;
 
                 // to figure out in which quadrant box is located
                 var diffHeight = centerY - (height - centerY);
                 var diffWidth = centerX - (width - centerX);
+                //  to figure out in which quadrant box is located (hamburger icon)
+                var diffHeightH = centerYH - (height - centerYH);
+                var diffWidthH = centerXH - (width - centerXH);
 
                 // distance to borders
                 var spaceRight = width-centerX;
                 var spaceBottom = height-centerY;
+                // distance to borders (hamburger icon)
+                var spaceRightH = width-centerXH;
+                var spaceBottomH = height-centerYH;
 
                 // positions to move the box
                 var rightBorder = width - nav.width() - 10;
                 var bottomBorder = height - nav.height() - 10;
                 //change behaviour when only hamburger is visible
+                var rightBorderH, bottomBorderH;
+                //change behaviour when only hamburger is visible
                 if(nav.hasClass('hide')) {
-                    rightBorder = width - nav.find('.nav-menu').width() -10;
-                    bottomBorder = height - nav.find('.nav-menu').height() -10;
+                    rightBorderH = width - nav.find('.nav-menu').width() -10;
+                    bottomBorderH = height - nav.find('.nav-menu').height() -10 ;
                 }
+
 
 
                 var animationTime = 400;
                 // could be done nicer with array + switch but it works!
-                if (diffHeight < 0 && diffWidth < 0) {
-                    // 1st quadrant
-                    if (centerX <= centerY) {
-                        nav.animate({
-                            left: "10"
-                        }, animationTime);
-                    } else {
-                        nav.animate({
-                            top: "10"
-                        }, animationTime);
+
+                if(nav.hasClass('hide')){
+
+                    if (diffHeightH < 0 && diffWidthH < 0) {
+                        // 1st quadrant
+                        if (centerXH <= centerYH) {
+                            nav.animate({
+                                left: "10"
+                            }, animationTime);
+                        } else {
+                            nav.animate({
+                                top: "10"
+                            }, animationTime);
+                        }
                     }
-                } else if (diffHeight < 0 && diffWidth > 0) {
-                    // 2nd quadrant
-                    if (spaceRight <= centerY) {
-                        nav.animate({
-                            left: rightBorder
-                        }, animationTime);
-                    } else {
-                        nav.animate({
-                            top: "10"
-                        }, animationTime);
+                    else if (diffHeightH < 0 && diffWidthH > 0) {
+                        // 2nd quadrant
+
+                        if (spaceRightH <= centerYH) {
+                            nav.animate({
+                                left: rightBorderH
+                            }, animationTime);
+                        } 	else {
+                            nav.animate({
+                                top: "10"
+                            }, animationTime);
+                        }
+
                     }
-                } else if (diffHeight > 0 && diffWidth > 0) {
-                    // 3rd quadrant
-                    if (spaceBottom <= spaceRight) {
-                        nav.animate({
-                            top: bottomBorder
-                        }, animationTime);
-                    } else {
-                        nav.animate({
-                            left: rightBorder
-                        }, animationTime);
+                    else if (diffHeightH > 0 && diffWidthH > 0) {
+                        // 3rd quadrant
+                        if (spaceBottomH <= spaceRightH) {
+                            nav.animate({
+                                top: bottomBorderH
+                            }, animationTime);
+                        } else {
+                            nav.animate({
+                                left: rightBorderH
+                            }, animationTime);
+                        }
                     }
-                } else if (diffHeight > 0 && diffWidth < 0) {
-                    // 4th quadrant
-                    if (centerX <= spaceBottom) {
-                        nav.animate({
-                            left: "10"
-                        }, animationTime);
-                    } else {
-                        nav.animate({
-                            top: bottomBorder
-                        }, animationTime);
+                    else if (diffHeightH > 0 && diffWidthH < 0) {
+                        // 4th quadrant
+                        if (centerXH <= spaceBottomH) {
+                            nav.animate({
+                                left: "10"
+                            }, animationTime);
+                        } else {
+                            nav.animate({
+                                top: bottomBorderH
+                            }, animationTime);
+                        }
+                    }     }
+
+                else {
+
+                    if (diffHeight < 0 && diffWidth < 0) {
+                        // 1st quadrant
+                        if (centerX <= centerY) {
+                            nav.animate({
+                                left: "10"
+                            }, animationTime);
+                        } else {
+                            nav.animate({
+                                top: "10"
+                            }, animationTime);
+                        }
+                    } else if (diffHeight < 0 && diffWidth > 0) {
+                        // 2nd quadrant
+
+                        if (spaceRight <= centerY) {
+                            nav.animate({
+                                left: rightBorder
+                            }, animationTime);
+                        } 	else {
+                            nav.animate({
+                                top: "10"
+                            }, animationTime);
+                        }
+
+                    } else if (diffHeight > 0 && diffWidth > 0) {
+                        // 3rd quadrant
+                        if (spaceBottom <= spaceRight) {
+                            nav.animate({
+                                top: bottomBorder
+                            }, animationTime);
+                        } else {
+                            nav.animate({
+                                left: rightBorder
+                            }, animationTime);
+                        }
+                    } else if (diffHeight > 0 && diffWidth < 0) {
+                        // 4th quadrant
+                        if (centerX <= spaceBottom) {
+                            nav.animate({
+                                left: "10"
+                            }, animationTime);
+                        } else {
+                            nav.animate({
+                                top: bottomBorder
+                            }, animationTime);
+                        }
                     }
+
                 }
             }
         };
@@ -120,7 +192,7 @@
             }
         });
 
-        nav.on('click', '.nav-menu', function () {
+        nav.on('dblclick', '.nav-menu', function () {
             nav.toggleClass('hide');
             if(!dragging){
                 makeAnimation()
@@ -163,10 +235,5 @@
                 makeAnimation();
             }
         })
-
-
-
-
-
     })
 }());
